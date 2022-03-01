1dafefcbcac18bed1b00fd3fb9955bb90d20eaad
Unai Fuentes <ufuentes@everis.com>
2022-03-01 20:54:04 +0100

    feat(Loading): add component

---
+++ src/components/Loading.js
@@ -0,0 +1,7 @@
+export const Loading = () => {
+  return (
+    <div className='flex flex-col h-screen items-center justify-center text-pink-500 font-bold'>
+      <span>Loading...</span>
+    </div>
+  );
+};
