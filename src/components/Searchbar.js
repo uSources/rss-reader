6bd240a757e944558c8759aa1bb5ca561f1c9b6c
Unai Fuentes <ufuentes@everis.com>
2022-03-01 20:00:05 +0100

    feat: conditional rendering

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -9,6 +9,7 @@ export const Searchbar = ({ setSearchtext }) => {
     setText(e.target.value);
     setSearchtext(e.target.value); //emit text to parent component
   };
+
   return (
     <input
       className='w-full p-2 bg-gray-100 caret-pink-500 text-black rounded-lg dark:bg-gray-900 dark:text-white'
