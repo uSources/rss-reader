7c4ef855e7664d71f1daab71bf4b12085bb4391f
Unai Fuentes <ufuentes@everis.com>
2022-03-01 19:58:51 +0100

    feat: show default image

--- src/components/PostImage.js
+++ src/components/PostImage.js
@@ -1,4 +1,14 @@
 export const PostImage = ({ thumbnail }) => {
+  if (!thumbnail) {
+    return (
+      <img
+        alt='thumbnail'
+        src='./not-found.jpg'
+        className='rounded-3xl w-72'
+      ></img>
+    );
+  }
+
   return (
     <img alt='thumbnail' src={thumbnail} className='rounded-3xl w-72'></img>
   );
